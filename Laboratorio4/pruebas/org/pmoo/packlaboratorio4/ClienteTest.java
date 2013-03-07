208fdd1a4ea8ff3e33a52795fed64cad38f28f24
samvrai <t3echelon@gmail.com>
2013-03-07 08:13:56 +0100

    Creados los JUnits

---
+++ Laboratorio4/pruebas/org/pmoo/packlaboratorio4/ClienteTest.java
@@ -0,0 +1,44 @@
+package org.pmoo.packlaboratorio4;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClienteTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEsPreferente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTieneMismoId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerSaldo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testActualizarSaldo() {
+		fail("Not yet implemented");
+	}
+
+}
