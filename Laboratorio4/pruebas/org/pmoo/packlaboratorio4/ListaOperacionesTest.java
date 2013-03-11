bed1e65dc601bcd05c075982342ed01bd397bc4e
chispas-elek <elektro108@gmail.com>
2013-03-11 19:37:03 +0100

    - Creados los nuevos archivos de Test para comprobar el resto de las clases

---
+++ Laboratorio4/pruebas/org/pmoo/packlaboratorio4/ListaOperacionesTest.java
@@ -0,0 +1,69 @@
+//Author -> Mulero Martínez, Rubén
+//Author -> Esteban García, Asier
+
+package org.pmoo.packlaboratorio4;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListaOperacionesTest {
+
+	//Atributos
+	ListaOperaciones lO1;
+	
+	@Before
+	public void setUp() throws Exception {
+		lO1 = ListaOperaciones.getListaOperaciones();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		lO1.resetear();
+		lO1 = null;
+	}
+
+	@Test
+	public void testGetListaOperaciones() {
+		boolean result = false;
+		if(lO1 != null) {
+			result = true;
+		}
+		assertTrue(result);
+	}
+
+	@Test
+	public void testCantidadOperaciones() {
+		lO1.anadirOperacion(111, 1, "tridente", 4);
+		assertEquals(1,lO1.cantidadOperaciones());
+	}
+
+	@Test
+	public void testAnadirOperacion() {
+		lO1.anadirOperacion(111, 1, "tridente", 4);
+		assertEquals(1,lO1.cantidadOperaciones());
+		//Metemos ésta segunda vez los mismos datos para comprobar que no nos deja meter el mismo ID de operación
+		lO1.anadirOperacion(111, 1, "tridente", 4);
+		assertEquals(1,lO1.cantidadOperaciones());
+	}
+
+	@Test
+	public void testRealizarOperaciones() {
+		lO1.anadirOperacion(111, 1, "tridente", 4);
+		ListaClientes l1 = ListaClientes.getListaClientes();
+		l1.anadirCliente(1,"Paco","tridente",12548894,true);
+		lO1.realizarOperaciones();
+		Cliente cl1 = l1.buscarClientePorId(1);
+		assertEquals(12548890,cl1.obtenerSaldo("tridente"),0.0001);
+	}
+
+	@Test
+	public void testResetear() {
+		lO1.anadirOperacion(111, 1, "tridente", 4);
+		lO1.resetear();
+		assertEquals(0,lO1.cantidadOperaciones());
+	}
+
+}
--- Laboratorio4/pruebas/org/pmoo/packlaboratorio4/OperacionTest.java
+++ Laboratorio4/pruebas/org/pmoo/packlaboratorio4/OperacionTest.java
@@ -12,28 +12,35 @@ import org.junit.Test;
 public class OperacionTest {
 
 	//Atributos
-	
+	Operacion op1;
 	@Before
 	public void setUp() throws Exception {
+		op1 = new Operacion(111,1,"tridente",4);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		op1 = null;
 	}
 
 	@Test
 	public void testOperacion() {
-		fail("No implementado aun");
+		assertTrue(op1.tieneMismoId(111));
 	}
 
 	@Test
 	public void testTieneMismoId() {
-		fail("No implementado aun");
+		assertTrue(op1.tieneMismoId(111));
 	}
 
 	@Test
 	public void testRealizarOperacion() {
-		fail("No implementado aun");
+		//Creamos una lista de clientes
+		ListaClientes l1 = ListaClientes.getListaClientes();
+		l1.anadirCliente(1,"Paco","tridente",12548894,true);
+		op1.realizarOperacion();
+		Cliente cl1 = l1.buscarClientePorId(1);
+		assertEquals(12548890,cl1.obtenerSaldo("tridente"),0.0001);
 	}
 
 }
