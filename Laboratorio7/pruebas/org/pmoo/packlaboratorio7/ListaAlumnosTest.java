385630bfd842c9e0ce352c59060f7d3186d576d3
samvrai <t3echelon@gmail.com>
2013-04-15 20:27:30 +0200

    Clases de prueba

---
+++ Laboratorio7/pruebas/org/pmoo/packlaboratorio7/ListaAlumnosTest.java
@@ -0,0 +1,49 @@
+package org.pmoo.packlaboratorio7;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListaAlumnosTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testListaAlumnos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAnadirAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetirarAlumno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEsta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTamano() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAlumnoEnPos() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Laboratorio7/pruebas/org/pmoo/packlaboratorio7/ListaParejasTest.java
@@ -0,0 +1,44 @@
+package org.pmoo.packlaboratorio7;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListaParejasTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetListaParejas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAnadirOrdenadoPareja() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObtenerParejaDe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReajustarParejas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetear() {
+		fail("Not yet implemented");
+	}
+
+}
