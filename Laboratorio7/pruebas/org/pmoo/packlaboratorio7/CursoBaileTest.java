de75e906893bc7cb88c55a6c540891f8ccdac7a6
samvrai <t3echelon@gmail.com>
2013-04-23 21:26:41 +0200

    Cursobaile

--- Laboratorio7/pruebas/org/pmoo/packlaboratorio7/CursoBaileTest.java
+++ Laboratorio7/pruebas/org/pmoo/packlaboratorio7/CursoBaileTest.java
@@ -8,12 +8,36 @@ import org.junit.Test;
 
 public class CursoBaileTest {
 
+	//Variables
+	private Hombre hom1,hom2;
+	private Mujer muj1,muj2;
+	private Pareja par1,par2;
+	private ListaParejas lParejas;
+	private ListaAlumnosCurso lAlums;
+	private CursoBaile curso;
+	
 	@Before
 	public void setUp() throws Exception {
+		hom1 = new Hombre("1111","Paco","Porras",31);
+		hom2 = new Hombre("1112","Poco","Parros",55);
+		muj1 = new Mujer("1113","Paca","Tal",33);
+		muj2 = new Mujer("1113","Poca","Tel",33);
+		par1 = new Pareja(hom1,muj1);
+		curso = CursoBaile.getCursoBaile();
+		lAlums = 
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		hom1 = null;
+		hom2 = null;
+		muj1 = null;
+		muj2 = null;
+		par1 = null;
+		par2 = null;
+		lParejas.resetear();
+		lAlums.resetear();
 	}
 
 	@Test
